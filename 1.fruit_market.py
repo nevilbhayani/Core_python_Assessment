cf22c8ba3f27a012fdef6c267b6de5ecd122e85d
nevilbhayani <nevilbhayani2212@gmail.com>
2024-01-04 15:04:36 +0530

    main

---
+++ 1.fruit_market.py
@@ -0,0 +1,54 @@
+def fruit_market():
+    print("\n\n\t\t\t\n\t\t\t\t\t\t\t* * * *  WELCOM TO FRUIT MARKET  * * * *\n\n")
+    print("\t\t\t\n\t\t\t\t\t\t\t\t 1] - Manager ")
+    print("\t\t\t\n\t\t\t\t\t\t\t\t 2] - Customer \n\n\n")
+
+
+    num1=int(input("Select Your Role :- "))
+    x=(" 1} - Add Fruit Stock ")
+    y=(" 2} - View Fruit Stock ")
+    z=(" 3} - Update Fruit Stock \n\n")
+
+    if num1 == 1:
+        print("\n\n\t\t\t\n\t\t\t\t\t\t\t\t* FRUIT MARKET MANAGER *\n")
+        print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t",x)
+        print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t",y)
+        print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t",z)
+
+    num2=int(input("\nEnter Your Choice :- "))
+    
+    if num2 == 1:
+        print("\n   \t\t\t\t\t\t\t\t  ~ ADD FRUIT STOCK ~\n")
+        name=str(input("\n Enter Fruit Name :- "))
+        qty=int(input(" Enter Qty (in Kg) :- "))
+        price=int(input("Enter Price :- "))
+        print("\n")
+        print("\n")
+
+        a=str(input(" Do you want to perfoem more operations : press 'y' for yes and 'n' for no :- "))
+        print("\n")
+        print("\n")
+
+        if a == "y" or a == "Y":
+            name1=str(input("Enter Fruit Name :- "))
+            qty1=int(input("Enter Qty (in Kg) :- "))
+            price1=int(input("Enter Price :- "))
+        elif a == "n" or a == "N":
+           print("\n   \t\t\t\t\t\t\t\t * * Thank You * *")
+
+    elif num2==2:
+     print("\n\n'",name,"'",{"QTY:",qty,'PRICE',price,"\n\n"})
+
+
+    num2=int(input("\nEnter Your Choice :- "))
+    if num2== 1:
+        name1=str(input("Enter Fruit Name :- "))
+        qty1=int(input("Enter Qty (in Kg) :- "))
+        price1=int(input("Enter Price :- "))
+    elif num2==2:
+        print("\n\n",name,{"QTY:",qty,"PRICE:",price})
+        print(name1,{"QTY:",qty1,"PRICE:",price1 })
+       
+
+fruit_market()
+
